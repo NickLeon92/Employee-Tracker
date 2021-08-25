e6dc074da9b3404300e06b0ef6fc1c2f2c4b4364
Nick Leon <nicolas7@vt.edu>
2021-08-25 13:04:26 -0400

    add employee/role/dept functionality

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const table = require('console.table')
 // Import and require mysql2
 const mysql = require('mysql2');
+let index;
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -143,34 +144,103 @@ const Menu = () => {
                 choices: arraytest,
             },
           ]).then((answers) => {
-            let index;
+            
+            
             db.query(`SELECT id from department where name = '${answers.deptChoice}'`, function (err, results){
-               console.log(results)
-               index = results[0].id
-               console.log(index)
-            })
-              
-            db.query(`INSERT INTO role(title, salary, department_id) VALUES(?)`, [answers.title, answers.salary , index] , (err, results) => {
+               
+               index = parseInt(results[0].id);
+               pls()
+            });
+            function pls () {
+            db.query(`INSERT INTO role(title, salary, department_id) VALUES(?,?,?)`, [answers.title, parseInt(answers.salary) , index] , (err, results) => {
+                console.log(index)
+                console.log(parseInt(index))
                 Menu();
-              });
+              });}
           }).catch((err) => console.error(err))
+              
+  }
+  const emplAdd = () => {
+    
+    const arraytest1 =[];
+    const arraytest2 =[];
+
+    db.query('SELECT title FROM role', function (err, results, field) {
+        // console.log(results)
+        results.forEach(element => {
+          arraytest1.push(element.title)
+        });
+        // console.log(arraytest1)
+    })
+    db.query(`SELECT id FROM employee WHERE manager_id IS NULL`, function (err, results, field) {
+      // console.log(results)
+      results.forEach(element => {
+        arraytest2.push(element.id)
+      });
+      arraytest2.push('None')
+      // console.log(arraytest2)
+    })  
+  
+      
+    return inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'Please Enter First Name:',
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Please Enter Last Name:',
+            },
+            {
+                type: 'list',
+                message: 'Please Select Role:',
+                name: 'roleChoice',
+                choices: arraytest1,
+            },
+            {
+                type: 'list',
+                message: 'Please Select Manager ID:',
+                name: 'manChoice',
+                choices: arraytest2,
+            }
+          ]).then(async (answers) => {
+
+            let index1;
+            let index2 = answers.manChoice;
+
+            if (answers.manChoice === 'None'){
+              index2 = null;
+            }
+            
+            db.query(`SELECT id from role where title = '${answers.roleChoice}'`, function (err, results){
+  
+              index1 = parseInt(results[0].id);
+              pls2()
+              
+            });
+
+            
+              function pls2() {
+              
+              db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)`, [answers.first_name, answers.last_name, index1, index2], (err, results) => {
+   
+                   Menu();
+                 });}
+            
+            
+            }
+
+          
+
+          
+        
+            
+              
+          ).catch((err) => console.error(err))
+              
   }
   
-
-  
-//   const emplAdd = () => {
-//     return inquirer.prompt(
-//         [
-//             {
-//               type: 'input',
-//               name: 'name',
-//               message: 'Please Enter Employee Name:',
-//             },
-//           ]).then((answers) => {
-//             db.query(`INSERT INTO employee(name) VALUES(?)`, answers.name, (err, results) => {
-//                 console.table(results);
-//               });
-//           })
-//   }
-
   Menu();
